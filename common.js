844d37c092004cbb2779bc018a264d509aad5831
dalton <dalton@oldfart.(none)>
2011-12-04 22:21:48 -0800

    Rewrote everything, check readme.

--- common.js
+++ common.js
@@ -1,58 +1,59 @@
 exports.conf = {
-	"auth": "auth+live+yourauth",
-	"userid": "userid",
-	"roomid": "roomid",
+	"auth": "fill this in your turntableUserAuth cookie",
+	"userid": "fill this in from your turntableUserId cookie",
+	"roomid": "fill this in from TURNTABLE_ROOMID in view-source:turntable.fm/$ROOM",
 	"greetings_filename": "greetings.json",
 	"pending_greetings_filename": "greetings-pending.json",
 	"activity_filename": "activity.json",
-    "rooms_filename": "rooms.json",
 	"usernames_filename": "usernames.json",
 	"djlist_filename": "djlist-{roomid}.json",
 	"banlist_filename": "banlist-{roomid}.json",
-    "owners": {
-        "4e0ff328a3f751670a084ba6":"YayRamen!",
-        "4e9a7d20a3f7515e6508de50":"Dj NastyBits",
-        "4e619cc9a3f7514df80f739c":"ZmbeePrncess"
-    },
+	"owners": {
+	},
+	"friends": {
+	},
 	"messages": {
-		"help": "The theme is {theme}, {queue}, {limit}.",
+		"help": "{theme}, {queue}, {limit}, {afk}.",
 		"defaultGreetings": [
-			"Hi {user.name}, welcome to {room.name}! Type *help to find out what's going on.",
-			"Hello {user.name}, welcome to {room.name}! Type *help to find out what's going on.",
-			"Hey there {user.name}. Welcome to {room.name}! Type *help to find out what's going on."
+			"Hi {user.name}!, welcome to {room.name}. Type 'help' to find out what's going on",
+			"Hello {user.name}, welcome to {room.name}. Type 'help' to find out what's going on",
+			"Hey there {user.name}, welcome to {room.name}. Type 'help' to find out what's going on"
 		],
 		"newUserGreetings": [
-			"Welcome to turntable {user.name}! Type *help to find out what's going on.",
-			"Hi {user.name}, welcome to turntable! Type *help to find out what's going on."
+			"Welcome to turntable {user.name}! Type 'help' to find out what's going on",
+			"Hi {user.name}, welcome to turntable! Type 'help' to find out what's going on"
 		],
 		"djAnnouncements": [
 			"{user.name} just started DJing with {user.points} points and {user.fans} fans.",
 		],
 		"newDjAnnouncements": [
-			"{user.name} has never spun before!",
+			"Now the real fun begins, {user.name}!",
 			"Show some love for {user.name}'s first time on the decks!"
 		],
 		"djSummaries": [
-			"Thanks for spinning, {user.name}.  You played {plays} songs and got {gain} points and {lames} lames."
+			"{user.name} played {plays} songs, gaining {gain} points (and got lamed {lames} times).",
+			"Thanks for the tunes {user.name}.  You played {plays} songs and got {gain} points and {lames} lames."
 		],
+        "theme": "Dubstep/Electro",
 		"plays": "{user.name} has played {plays} songs.",
-		"newModerator": "{user.name} is now a moderator in {room.name}.  Don't blow it.",
-		"songSummary": "{song} earned {user.name} {awesomes} points!",
+		"newModerator": "{user.name} is now a moderator in {room.name}.  With great power comes great responsibility.",
+		"songSummary": "{song} by {artist}: +{awesomes}, -{lames}",
 		"album": "{song} is on {album}",
 		"bonus": "{dj.name} just got a bonus from {user.name}",
 		"bonusAlreadyUsed": "Sorry, bonus already used by {user.name}.  My head is already boppin!",
+		"selfBonus": "Sorry, {user.name}, you can't give yourself a bonus.",
 		"lastActivity": "It's been {age} since I saw {user.name}",
 		"lastActivityUnknown": "Hmm, I haven't seen {user.name}",
-		"listInactive": "There is no queue.  Free for All to get on deck.",
-		"listEmpty": "Nobody's called next spot.  Type *addme to call it.",
-		"listAdded": "{user.name}, you're now #{position} on the queue.",
-		"list": "The current dj queue is: {list}",
-		"listOn": "The room now has a dj queue.  Type *addme to call the next spot.",
-		"listOff": "The dj queue is now off.  Free for all to get on deck.",
-		"listAlreadyOn": "The dj queue is on already.  Type *list to see the current queue.",
-		"listAlreadyOff": "The dj queue is already off.  Free for all.",
-		"listAlreadyListed": "{user.name} is already #{position} on the queue.",
-		"listRemoved": "{user.name} has given up his spot in the queue.",
+		"listInactive": "There is no list.  Fastest-fingers.",
+		"listEmpty": "Nobody's on the list.  Type *addme to add yourself.",
+		"listAdded": "{user.name}, you're now #{position} on the list.",
+		"list": "The current dj list is: {list}",
+		"listOn": "The room now has a dj list.  Type *addme to add yourself.",
+		"listOff": "The dj list is now off.  Fastest-fingers.",
+		"listAlreadyOn": "The dj list is already on.  Type *list to see the current list.",
+		"listAlreadyOff": "The dj list is already off.  Fastest-fingers.",
+		"listAlreadyListed": "{user.name} is already #{position} on the list.",
+		"listRemoved": "{user.name} has been removed from the DJ list.",
 		"listRemoveNotListed": "{user.name} is not on the DJ list.",
 		"listReset": "The dj list has been cleared.",
 		"banned": "{user.name} is banned: {ban.comment}",
@@ -62,7 +63,6 @@ exports.conf = {
 		"bans": "{ban.count} banned users: {ban.list}",
 		"wrongDj": "{right.name} is supposed to be up next.  {wrong.name}, please step down.",
 		"nextDj": "{user.name} is up next.",
-		"unknownUser": "Sorry, can't find anybody by the name of {user.name}",
 		"someMessage": "some-message-value",
 		"greetingRejected": "Greeting for {user.name} removed.",
 		"pendingGreetingRejected": "Pending greeting for {user.name} rejected.",
